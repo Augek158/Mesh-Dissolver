e354963e7f0e4451c1c26a2a58d345335c29f136
August Nam-Ki Ek <augek158@student.liu.se>
2014-11-18 23:07:16 +0100

    Added 'Hello Maya' that builds successfully

---
+++ include/HelloWorld.h
@@ -0,0 +1,16 @@
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+ 
+#include <maya/MArgList.h>
+#include <maya/MGlobal.h>
+#include <maya/MPxCommand.h>
+#include <maya/MObject.h>
+ 
+class HelloWorld : public MPxCommand {
+ public:
+  HelloWorld() {};
+  virtual MStatus doIt(const MArgList& argList);
+  virtual MStatus redoIt();
+  static void* creator();
+};
+#endif
---
+++ src/HelloWorld.cpp
@@ -0,0 +1,65 @@
+#include "HelloWorld.h"
+
+#include <maya/MFnPlugin.h>
+#include <maya/MString.h>
+#include <maya/MArgList.h>
+#include <maya/MSelectionList.h>
+#include <maya/MItSelectionList.h>
+#include <maya/MPoint.h>
+#include <maya/MVector.h>
+#include <maya/MDagPath.h>
+ 
+void* HelloWorld::creator() { return new HelloWorld; }
+ 
+MStatus HelloWorld::doIt(const MArgList& argList) {
+
+    MVector vector( 1.0, 0.0, 0.0 );
+    MStatus stat;
+    MSelectionList selection;
+    MGlobal::getActiveSelectionList(selection);
+    MItSelectionList iter(selection, MFn::kInvalid, &stat );
+
+    if (MS::kSuccess == stat) {
+        MDagPath mdagPath; 
+        MObject mComponent; 
+        MGlobal::displayInfo("Created Object.");
+        
+        for (; !iter.isDone(); iter.next()) {
+            iter.getDagPath(mdagPath, mComponent);
+        }
+    }
+
+
+    redoIt();
+
+    return MS::kSuccess;
+}
+
+MStatus HelloWorld::redoIt (){
+
+    // Do actual work here with the data from local class.
+    
+    return MS::kSuccess;
+}
+ 
+MStatus initializePlugin(MObject obj) {
+    MFnPlugin plugin(obj, "August Ek & Ramin Assadi", "0.1", "Any");
+    MStatus status = plugin.registerCommand("helloWorld", HelloWorld::creator);
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+    return status;
+}
+ 
+MStatus uninitializePlugin(MObject obj) {
+    MFnPlugin plugin(obj);
+    MStatus status = plugin.deregisterCommand("helloWorld");
+    CHECK_MSTATUS_AND_RETURN_IT(status);
+    return status;
+}
+
+int main(int argc, char** argv) {
+
+    if (argc != 2) return 0;
+
+    return 0;
+
+}
