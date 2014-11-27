c452fce4df4069ad97972e5910e31f92201dc380
August Nam-Ki Ek <augek158@student.liu.se>
2014-11-27 11:30:53 +0100

    Renaming files and functions
    * Changed names from HelloWorld -> MeshDissolver.
    * Function is now called with 'dissolve' instead of 'helloWorld' from Maya.

--- include/MeshDissolver.h
+++ include/MeshDissolver.h
@@ -1,14 +1,14 @@
-#ifndef HELLOWORLD_H
-#define HELLOWORLD_H
+#ifndef MESH_DISSOLVER_H
+#define MESH_DISSOLVER_H
  
 #include <maya/MArgList.h>
 #include <maya/MGlobal.h>
 #include <maya/MPxCommand.h>
 #include <maya/MObject.h>
  
-class HelloWorld : public MPxCommand {
+class MeshDissolver : public MPxCommand {
  public:
-   HelloWorld() {};
+   MeshDissolver() {};
    virtual MStatus doIt(const MArgList& argList);
    virtual MStatus redoIt();
    static void* creator();
--- src/MeshDissolver.cpp
+++ src/MeshDissolver.cpp
@@ -1,17 +1,18 @@
-#include "HelloWorld.h"
+#include "MeshDissolver.h"
 
 #include <maya/MFnPlugin.h>
 #include <maya/MString.h>
 #include <maya/MArgList.h>
 #include <maya/MSelectionList.h>
 #include <maya/MItSelectionList.h>
+#include <maya/MItMeshVertex.h>
 #include <maya/MPoint.h>
 #include <maya/MVector.h>
 #include <maya/MDagPath.h>
  
-void* HelloWorld::creator() { return new HelloWorld; }
+void* MeshDissolver::creator() { return new MeshDissolver; }
  
-MStatus HelloWorld::doIt(const MArgList& argList) {
+MStatus MeshDissolver::doIt(const MArgList& argList) {
 
     MVector vector( 1.0, 0.0, 0.0 );
     MStatus stat;
@@ -22,20 +23,31 @@ MStatus HelloWorld::doIt(const MArgList& argList) {
     if (MS::kSuccess == stat) {
         MDagPath mdagPath; 
         MObject mComponent; 
-        MGlobal::displayInfo("Created Object.");
         
         for (; !iter.isDone(); iter.next()) {
             iter.getDagPath(mdagPath, mComponent);
+
+            MItMeshVertex vtxFn( mdagPath, mComponent, &stat );
+            if ( MS::kSuccess == stat ) { // Vertex iterator successfully initalized.
+                for ( ; !vtxFn.isDone(); vtxFn.next() ) {
+                     if ( MS::kFailure == vtxFn.translateBy( vector ) ) {
+                         cerr << "Error setting Vertex\n";
+                     }
+                 }
+                 vtxFn.updateSurface();
+            } 
         }
+    } else {
+        cerr << "Failed to create selection iterator." << endl;
     }
 
 
-    redoIt();
+
 
     return MS::kSuccess;
 }
 
-MStatus HelloWorld::redoIt (){
+MStatus MeshDissolver::redoIt (){
 
     // Do actual work here with the data from local class.
     
@@ -44,14 +56,14 @@ MStatus HelloWorld::redoIt (){
  
 MStatus initializePlugin(MObject obj) {
     MFnPlugin plugin(obj, "August Ek & Ramin Assadi", "0.1", "Any");
-    MStatus status = plugin.registerCommand("helloWorld", HelloWorld::creator);
+    MStatus status = plugin.registerCommand("dissolve", MeshDissolver::creator);
     CHECK_MSTATUS_AND_RETURN_IT(status);
     return status;
 }
  
 MStatus uninitializePlugin(MObject obj) {
     MFnPlugin plugin(obj);
-    MStatus status = plugin.deregisterCommand("helloWorld");
+    MStatus status = plugin.deregisterCommand("dissolve");
     CHECK_MSTATUS_AND_RETURN_IT(status);
     return status;
 }
