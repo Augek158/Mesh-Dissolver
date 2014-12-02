a6f6c9e16770faacffcf141b9037809c094fad3f
ramas008 <ramintt1@hotmail.com>
2014-12-02 11:17:20 +0100

    Added translation functions it is possible to translate each mesh by a vector it is also possible to translate each face by a vector

--- src/MeshDissolver.cpp
+++ src/MeshDissolver.cpp
@@ -7,40 +7,63 @@
 #include <maya/MItSelectionList.h>
 #include <maya/MItMeshVertex.h>
 #include <maya/MPoint.h>
+#include <maya/MPointArray.h>
 #include <maya/MVector.h>
 #include <maya/MDagPath.h>
+#include <maya/MItMeshPolygon.h>
+#include <iostream>
+#include <string>
+
+#include <time.h>
  
 void* MeshDissolver::creator() { return new MeshDissolver; }
  
 MStatus MeshDissolver::doIt(const MArgList& argList) {
-
+	clock_t t = clock();
     MVector vector( 1.0, 0.0, 0.0 );
     MStatus stat;
+
     MSelectionList selection;
     MGlobal::getActiveSelectionList(selection);
-    MItSelectionList iter(selection, MFn::kInvalid, &stat );
+	MItSelectionList iter(selection, MFn::kInvalid, &stat);
+
+	//MGlobal::executeCommand("move -r 0.0 30.0 0.0");
 
     if (MS::kSuccess == stat) {
         MDagPath mdagPath; 
-        MObject mComponent; 
-        
+        MObject mComponent;
+		MVector vector = MVector(0.0, 30.0, 0.0);
+
+        // Loop through each mesh
         for (; !iter.isDone(); iter.next()) {
             iter.getDagPath(mdagPath, mComponent);
 
-            MItMeshVertex vtxFn( mdagPath, mComponent, &stat );
-            if ( MS::kSuccess == stat ) { // Vertex iterator successfully initalized.
-                for ( ; !vtxFn.isDone(); vtxFn.next() ) {
-                     if ( MS::kFailure == vtxFn.translateBy( vector ) ) {
-                         cerr << "Error setting Vertex\n";
-                     }
-                 }
-                 vtxFn.updateSurface();
-            } 
-        }
-    } else {
-        cerr << "Failed to create selection iterator." << endl;
-    }
+			// Translate each mesh by vector
+			MItMeshVertex vertexIter(mdagPath);
+			for (; !vertexIter.isDone(); vertexIter.next()) {
+				vertexIter.translateBy(vector);
+			}
+			
+			// Translate each face in a mesh
+			/*MItMeshPolygon faceIter(mdagPath);
+			if (MS::kSuccess == stat) { 
+				for (; !faceIter.isDone(); faceIter.next()) {
+					MPointArray pointArray;
+					faceIter.getPoints(pointArray);
+					for (unsigned int i = 0; i < pointArray.length(); i++) {
+						pointArray[i].x += vector.x;
+						pointArray[i].y += vector.y;
+						pointArray[i].z += vector.z;
+					}
+					faceIter.setPoints(pointArray);
 
+					faceIter.updateSurface();
+				}
+			}*/
+        }
+    } 
+	t = clock() - t;
+	cout << ((float)t) / CLOCKS_PER_SEC << endl;
     return MS::kSuccess;
 }
 
